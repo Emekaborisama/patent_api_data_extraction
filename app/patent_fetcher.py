49964832df19ab847f6fb030c241fcd5b1ec6add
emekaborisama <borisphilosophy@gmail.com>
2023-09-18 01:16:04 +0000

    adjustment based on comment

--- app/patent_fetcher.py
+++ app/patent_fetcher.py
@@ -15,26 +15,26 @@ except:
 
 my_logger = get_logger("uspto_bulk_search_download")
 # Constants should be all caps
-output_folder = "data"
-os.makedirs(output_folder, exist_ok=True)
+OUTPUT_FOLDER = "data"
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 API_URL = "https://developer.uspto.gov/ibd-api/v1/application/grants"
 
 
 # Not using correct PEP8 python styinlg for class name.
-class uspto_bulk_search:
-    def __init__(self, grantFromDate: str, grantToDate: str, rows: int) -> None:
+class UsptoBulkSearch:
+    def __init__(self, grant_from_date: str, grant_to_date: str, rows: int) -> None:
         """Initialize a USPTO Bulk Search object.
 
         Args:
-            grantFromDate: The starting date for patent grants in the format 'YYYY-MM-DD'.
-            grantToDate: The ending date for patent grants in the format 'YYYY-MM-DD'.
+            grant_from_date: The starting date for patent grants in the format 'YYYY-MM-DD'.
+            grant_to_date: The ending date for patent grants in the format 'YYYY-MM-DD'.
             rows: The number of rows to request per API call.
         """
-        self.grantFromDate = grantFromDate
-        self.grantToDate = grantToDate
+        self.grant_from_date = grant_from_date
+        self.grant_to_date = grant_to_date
         self.rows = rows
 
-    def request_data(self, start_size: int, results_dict: Optional[dict] = None):
+    def get_request_data(self, start_size: int, results_dict: Optional[dict] = None):
         """Make a request to the USPTO API to retrieve patent data.
 
         Args:
@@ -44,7 +44,7 @@ class uspto_bulk_search:
         Returns:
             list or None: A list of dictionaries containing patent data or None if no results are found.
         """
-        url = f"{API_URL}?grantFromDate={self.grantFromDate}1&start={start_size}&grantToDate={self.grantToDate}&rows={self.rows}"
+        url = f"{API_URL}?grantFromDate={self.grant_from_date}1&start={start_size}&grantToDate={self.grant_to_date}&rows={self.rows}"
 
         c = pycurl.Curl()
         data = BytesIO()
@@ -93,7 +93,7 @@ class uspto_bulk_search:
             c.close()
 
     # Define a separate function for mapping to work around pickling issue
-    def map_request_data(self, args: Tuple[int, Optional[dict]]):
+    def map_request_data(self, args: Tuple[int, Optional[dict]]) -> Optional[list[dict]]:
         """Map function for parallel processing of request_data.
 
         Args:
@@ -102,18 +102,18 @@ class uspto_bulk_search:
         Returns:
             list or None: A list of dictionaries containing patent data or None if no results are found.
         """
-        return self.request_data(*args)
+        return self.get_request_data(*args)
 
-    def run_parallel_requests(self) -> None:
+    def run_request_data(self) -> None:
         """Run parallel requests to retrieve patent data and save it to a CSV file."""
         start_size = 0
         num_processes = 20
 
         with Manager() as manager:
             results_dict = manager.dict()  # Create a shared dictionary for results
-            has_results = True
+            pool_results = True
 
-            while has_results:
+            while pool_results:
                 # Prepare a list of arguments for request_data
                 args_list = [
                     (x, results_dict)
@@ -127,7 +127,7 @@ class uspto_bulk_search:
                     p.map(self.map_request_data, args_list)
 
                 # Check if any of the processes retrieved results
-                has_results = any(
+                pool_results = any(
                     results_dict.get(start_size, None) is not None
                     for start_size in range(
                         start_size, start_size + num_processes * 100, 100
@@ -148,7 +148,7 @@ class uspto_bulk_search:
             # say pull all the patents for 2 years, this file would be hundreds of GB in
             # size which would make it impossible to open the file in a python process. It also makes the process of writing to it across different
             # processes challenging.
-            output_file_path = os.path.join(output_folder, "export.csv")
+            output_file_path = os.path.join(OUTPUT_FOLDER, "export.csv")
             df.to_csv(output_file_path)
             # Why would there be duplicates?
             dup_rows = df[df.duplicated(keep=False)]
@@ -162,7 +162,7 @@ if __name__ == "__main__":
     start_date = os.getenv("START_DATE")
     end_date = os.getenv("END_DATE")
 
-    bulk_search = uspto_bulk_search(
-        grantFromDate=start_date, grantToDate=end_date, rows=100
+    bulk_search = UsptoBulkSearch(
+        grant_from_date=start_date, grant_to_date=end_date, rows=100
     )
-    bulk_search.run_parallel_requests()
+    bulk_search.run_request_data()
